dd7166c1788b6b57b14a3ee4dc69df726afcc785
JoseRandoDEV <joseandresrando@gmail.com>
2025-08-05 18:04:59 -0300

    Clase 3: Migraciones y Modelos

---
+++ modeldata/app/Models/Note.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+/*    protected $table = 'notes';
+
+    protected $fillable = [    // campos asignables
+        'title',
+        'description',
+        'deadline',
+        'done',
+    ];
+
+    protected $guarded = [    //campos protegidos
+    ];
+
+    protected $casts = [     // tipos de datos
+        'deadline' => 'date',
+        'done' => 'boolean',
+    ];
+
+    protected $hidden = [
+        'password'          //campos ocultos
+    ];
+*/
+}
