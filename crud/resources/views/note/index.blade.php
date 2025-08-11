6c7117040f89a99c6e6f3f37e5c8a4f6f6d16624
JoseRandoDEV <joseandresrando@gmail.com>
2025-08-11 19:53:27 -0300

    Clase 5: CRUD

---
+++ crud/resources/views/note/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<a href="{{ route('note.create') }}">Create New Note</a>
+<ul>
+    @forelse($notes as $note)
+        <li>
+            <a href="{{ route('note.show', $note->id) }}">{{ $note->title }}</a> |
+            <a href="{{ route('note.edit', $note->id) }}">Edit</a> |
+            <form method="POST" action="{{ route('note.destroy', $note->id) }}">
+                {{-- Use POST method with DELETE --}}
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="DELETE" />
+            </form>
+        </li>
+    @empty
+        <p>No data.</p>
+    @endforelse
+</ul>
+@endsection
