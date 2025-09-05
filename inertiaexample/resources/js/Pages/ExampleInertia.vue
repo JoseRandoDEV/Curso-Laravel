5688acdfd23f82e66fc96a0527e4ec215fee89dd
JoseRandoDEV <joseandresrando@gmail.com>
2025-09-05 08:34:21 -0300

    Clase 12: Inertia

---
+++ inertiaexample/resources/js/Pages/ExampleInertia.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{ counter }}</h1>
+    <button @click="increment">Increment</button><br>
+    <NavLink :href="route('login')">Login</NavLink>
+</template>
+
+<script setup>
+import NavLink from '@/Components/NavLink.vue';
+import { ref } from 'vue';
+    const counter = ref(0);
+    const increment = () => {
+        counter.value++;
+    };
+</script>
+
+<style scoped>
+
+</style>
+
+// npm run dev
+// php artisan serve
+
